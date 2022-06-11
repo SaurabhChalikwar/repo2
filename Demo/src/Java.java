3e36600854ba2cc4a78bd2e2900b8f0c37a42360
Saurabh Chalikwar <saurabhchalikwar1@gmail.com>
2022-06-11 22:55:06 +0530

    after changes

--- Demo/src/Java.java
+++ Demo/src/Java.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -8,23 +9,13 @@ public class Java {
 
 	public static void main(String[] args) {
 
-		Map<String, Integer> map=new HashMap<>();
+		ArrayList<Integer> list=new ArrayList<Integer>();
 		
-		map.put("saurabh", 10);
-		map.put("parbhani", 20);
-		map.put("latur", 30);
-		map.put("pune", 40);
-		map.put("mumbai", 50);
+		list.add(10);
+		list.add(20);
+		list.add(30);
 		
-		//Entry<String, Integer> entry=(Entry<String, Integer>) map.entrySet();
-		
-		Set<Entry<String, Integer>> set=map.entrySet();
-	 
-		for(Entry<String, Integer> s:set) {
-			System.out.println(s.getKey()+"  "+s.getValue());
-		}
-		
-	
+		list.stream().map(i->i+2).forEach(System.out::println);
 		
 	}
 
