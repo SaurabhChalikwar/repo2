2b9010f531c1dd53106497b2d0b181f5af3c88c9
Saurabh Chalikwar <saurabhchalikwar1@gmail.com>
2022-06-24 20:45:21 +0530

    created new class as Print and used main method to print massage "inside print class"

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,24 +1,42 @@
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import javax.management.Query;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Ege.ageBreakOrNot(31);
+	}
+}
 
-		List<Integer> list=new ArrayList<>();
+
+class Ege{
+	
+	public static void ageBreakOrNot(int flore) {
+	
 		
-		list.add(10);
-		list.add(13);
-		list.add(14);
-		list.add(18);
-		list.add(14);
+		for(int i=0;i<=100;i++) {
+			if(i>=32 && i<=100) {
+				System.out.println("eeg will break at the flore : "+i);
+			}else {
+				System.out.println("egg will not break at flore : "+i);
+			}
+		}
 		
-		List<Integer> list2 = list.stream().filter(i->(i%2==0)).collect(Collectors.toList());
-		
-		list2.forEach(System.out::println);
 		
 	}
-
 }
