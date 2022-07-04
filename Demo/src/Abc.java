991462ec435066c0d1c32bca0f67d8e2abf53ec6
Saurabh Chalikwar <saurabhchalikwar1@gmail.com>
2022-07-04 17:15:52 +0530

    implimented for loop to print index value from 0 to 10.

--- Demo/src/Abc.java
+++ Demo/src/Abc.java
@@ -2,10 +2,9 @@ public class Abc{
   
  public static void main(String [] args){
    
-  System.out.println("group R"); 
-  
-  System.out.println("java developers");
-   
+  for(int i=0;i<=10;i++) {
+	  System.out.println(i);
+  }
  }
   
 }
